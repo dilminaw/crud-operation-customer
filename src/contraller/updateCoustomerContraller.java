02cdf5cb6203df6c35fc5a43b98cd0275c4739a3
dilminaw <dilminadilshan9@gmail.com>
2025-05-19 15:38:02 +0530

    update Search Form

--- src/contraller/updateCoustomerContraller.java
+++ src/contraller/updateCoustomerContraller.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.math.BigDecimal;
+import java.sql.*;
+
 public class updateCoustomerContraller {
 
     @FXML
@@ -21,6 +24,57 @@ public class updateCoustomerContraller {
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
 
+
+
+
+
+
+        String sql = "UPDATE customer SET name=?, address=?, sallary=? WHERE id=?";
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/thogakade","root","1234");
+
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+         //   PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, txtName.getText());
+            ps.setString(2, txtAddress.getText());
+            ps.setBigDecimal(3, new BigDecimal(txtsallary.getText()));
+            ps.setString(4, txtID.getText());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
-}
+ public void BTNsearchOnAction(ActionEvent actionEvent) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/thogakade", "root", "1234");
+
+            // Correct SQL query with parameter placeholder
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer WHERE id = ?");
+            preparedStatement.setString(1, txtID.getText());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                txtName.setText(resultSet.getString("name"));
+                txtAddress.setText(resultSet.getString("address"));
+                txtsallary.setText(String.valueOf(resultSet.getDouble("sallary")));
+            } else {
+                // Optional: handle case where ID is not found
+                txtName.setText("");
+                txtAddress.setText("");
+                txtsallary.setText("");
+                System.out.println("No customer found with ID: " + txtID.getText());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+  }
+    }
+
